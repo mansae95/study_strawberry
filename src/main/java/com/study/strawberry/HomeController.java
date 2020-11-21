ba21ec2029f23022f45bde3532efb38228f5b4d9
95grit <95grit@gmail.com>
2020-11-21 13:14:39 +0900

    2st commit

--- src/main/java/com/study/strawberry/HomeController.java
+++ src/main/java/com/study/strawberry/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	// test
+	// test2
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
