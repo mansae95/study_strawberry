d8941dc7246df8b1e98b25a52d2bf61f80680557
95grit <95grit@gmail.com>
2020-11-24 01:26:29 +0900

    [mansae] 메인페이지, 사이드바, 푸터 작업

--- src/main/java/com/study/strawberry/HomeController.java
+++ src/main/java/com/study/strawberry/HomeController.java
@@ -1,40 +1,32 @@
 package com.study.strawberry;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 * @throws Exception 
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) throws Exception {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-
-		return "home";
+	@RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 혹시 나중에 메인 이미지를 슬라이드형식으로 편집할 수 있음으로 받아오기 쉽게 ModelAndView타입 사용.
+		return new ModelAndView("home/home");
 	}
 	
+	//footer와 side bar 적용에 관한 Example.
+	@RequestMapping(value = "example", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView footerAndSidebarExample(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		/* 로그인 세션 체크용, 나중에 로그인 체크시 세션네임 변경해야함. (테스트용)
+		 * 주석 없애고 example 페이지로 가면 사이드바에 SIGN IN / SIGN UP에서 SIGN OUT / MY PAGE로 변경되는걸 확인하실 수 있음. */
+		/*
+		HttpSession session = request.getSession();
+		session.setAttribute("loginTest", true);
+		*/
+		return new ModelAndView("example/example");
+	}
 }
