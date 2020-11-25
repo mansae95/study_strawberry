fe3fcc9205645238d9910357552a191048e0078d
jh Lim <jhwlim25@gmail.com>
2020-11-25 23:03:46 +0900

    Update agreement.jsp for signup page 1

--- src/main/java/com/study/strawberry/test/controller/TestController.java
+++ src/main/java/com/study/strawberry/test/controller/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TestController {
 	
 	// Test Web 페이지
+	// 회원가입 약관동의 페이지 체크를 위한 테스트 컨트롤러
+	// 추후 삭제 필요
 	@RequestMapping(value = "/member/signup", method = RequestMethod.GET)
 	public String signup() {
 		System.out.println("URL : /member/signup/agreement - GET");
