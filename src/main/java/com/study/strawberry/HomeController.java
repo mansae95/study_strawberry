60864bf01ac81183fef511ab63cdccc93f890936
95grit <95grit@gmail.com>
2020-11-27 08:01:25 +0900

    asd

--- src/main/java/com/study/strawberry/HomeController.java
+++ src/main/java/com/study/strawberry/HomeController.java
@@ -2,7 +2,6 @@ package com.study.strawberry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,19 +13,6 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
 	public ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// 혹시 나중에 메인 이미지를 슬라이드형식으로 편집할 수 있음으로 받아오기 쉽게 ModelAndView타입 사용.
 		return new ModelAndView("home/home");
-	}
-	
-	//footer와 side bar 적용에 관한 Example.
-	@RequestMapping(value = "example", method = {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView footerAndSidebarExample(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		/* 로그인 세션 체크용, 나중에 로그인 체크시 세션네임 변경해야함. (테스트용)
-		 * 주석 없애고 example 페이지로 가면 사이드바에 SIGN IN / SIGN UP에서 SIGN OUT / MY PAGE로 변경되는걸 확인하실 수 있음. */
-		/*
-		HttpSession session = request.getSession();
-		session.setAttribute("loginTest", true);
-		*/
-		return new ModelAndView("example/example");
 	}
 }
